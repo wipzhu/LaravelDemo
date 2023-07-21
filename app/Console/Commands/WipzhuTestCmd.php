2dd7d01b1f118d92d48962926860e11c64d1171e
wipzhu <wipzhu@qq.com>
2023-07-21 18:55:12 +0800

    测试脚本

--- app/Console/Commands/WipzhuTestCmd.php
+++ app/Console/Commands/WipzhuTestCmd.php
@@ -40,6 +40,49 @@ class WipzhuTestCmd extends Command
      */
     public function handle()
     {
+        $name = $this->ask('Your username is?');
+        $this->info('Your name is：' . $name);
+
+        $password = $this->secret('Your password is?');
+        $this->info('Your password is：' . $password);
+
+        $continue = $this->confirm('Do you want to continue?');
+        if ($continue) {
+            $this->info('yes, continue');
+        } else {
+            $this->warn('no, break');
+        }
+
+        $city = $this->choice('Your city is?', ['Beijing', 'Shanghai', 'Shenzhen']);
+        $this->info('Your city is：' . $city);
+
+        $headers = ['name', 'password', 'city'];
+        $users = [
+            [
+                'name' => $name,
+                'password' => $password,
+                'city' => $city
+            ]
+        ];
+        $this->table($headers, $users);
+
+        // 获取指定参数
+        if ($this->hasArgument('name')) {
+            // 获取所有参数 $args = $this->arguments()
+            $arg = $this->argument('name');
+            if (!is_null($arg)) {
+                $this->info($arg);
+            }
+        }
+        // 获取指定选项
+        if ($this->hasOption('id')) {
+            // 获取所有选项 $options = $this->options()
+            $option = $this->option('id');
+            if (!is_null($option)) {
+                $this->info($option);
+            }
+        }
+        die();
         $s = "Hello World";
         $this->line($s);
         $this->comment($s);
